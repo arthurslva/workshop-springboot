0b240616a7d42734b106bf28ccf0627a3dc05b74
arthurslva <arthur.ajkl@gmail.com>
2025-04-04 16:21:56 -0300

    Classe Category

---
+++ src/main/java/com/educandoweb/course/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	public CategoryRepository category;
+	
+	public List<Category> findAll(){
+		return category.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = category.findById(id);
+		return obj.get();
+	}
+
+}
