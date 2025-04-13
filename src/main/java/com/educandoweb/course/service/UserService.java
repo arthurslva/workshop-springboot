61e91a16d77ae31bbf036e9e4b30d76de71dc5da
arthurslva <arthur.ajkl@gmail.com>
2025-04-13 16:58:19 -0300

    Métodos de Insert, delete e update de usuário

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -25,4 +25,25 @@ public class UserService {
 		return obj.get();
 	}
 
+	public User insert(User obj) {
+		return Repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		Repository.deleteById(id);
+
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = Repository.getReferenceById(id);
+		updateData(entity, obj);
+		return Repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
